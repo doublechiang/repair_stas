574da0dbd538e6ff216db59a24f2cdbce9efeb4d
Lawrence Dizon <lawrence.dizon@qmfremont.com>
2023-01-30 09:11:01 -0800

    Multiprocessing & SSH added for Mac Addr Parsing

--- settings.py
+++ settings.py
@@ -1,9 +1,23 @@
+# """ All module are singleton
+#     To use: # import settings
+# """
+
+
 """ All module are singleton
     To use: # import settings
 """
+lease_file = '/var/lib/dhcp/dhcpd.leases'
+hostname = '192.168.204.169'
+webssh = {'host' : f'http://{hostname}:8888'}
 
-webssh = { 'host' : 'http://192.168.66.53:8888' }
 
-mgmt_switch = '10.16.0.2 10.16.0.5'.split()
+
+mgmt_switch = [
+        ['10.16.0.7','SSH',{99 : 'Example'},''],
+        ['10.16.0.2','TN',{99 : 'Example'}, ''],
+        ['10.16.0.8','SSH',{99 :'Example'},'']
+        ]
+
+
 
 
